7e2712ffbea8ad3f19b54058d5799aef30cb2ed9
kselk <kselker@gmail.com>
2015-04-02 14:12:08 -0600

    initial commit

---
+++ Calc/CalcBrain.swift
@@ -0,0 +1,154 @@
+//
+//  CalcBrain.swift
+//  Calc
+//
+//  Created by eduardo on 3/5/15.
+//  Copyright (c) 2015 eduardo. All rights reserved.
+//
+
+
+import Foundation
+
+class CalcBrain : Printable
+{
+    
+    var description: String {
+        get {
+            return "\(opStack)"
+        }
+    
+    }
+    
+    private enum Op : Printable
+    {
+        case Operand(Double)
+        case Variable(String)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        case ConstantOperation(String, () -> Double)
+        
+        var description: String {
+            get {
+                switch  self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                case .ConstantOperation(let symbol, _):
+                    return symbol
+                case .Variable(let variable):
+                    return variable
+                }
+            }
+        }
+    }
+    
+    
+    private var opStack = [Op]()                        // array
+    
+    private var knownOps = [String: Op]()               // dictionary
+    
+    init()
+    {
+                func learnOp (op: Op){
+                    knownOps[op.description] = op
+                }
+//        
+//        knownOps["×"] = Op.BinaryOperation("×", *)
+//        knownOps["÷"] = Op.BinaryOperation("÷") {$1/$0}
+//        knownOps["+"] = Op.BinaryOperation("+", +)
+//        knownOps["−"] = Op.BinaryOperation("-") {$1 - $0}
+        
+                learnOp(Op.BinaryOperation("×", *))
+                learnOp(Op.BinaryOperation("÷") {$1/$0})
+                learnOp(Op.BinaryOperation("+", +))
+                learnOp(Op.BinaryOperation("-") {$1 - $0})
+        
+                learnOp(Op.UnaryOperation("√", sqrt))
+                learnOp(Op.UnaryOperation("sin", sin))
+                learnOp(Op.UnaryOperation("cos", cos))
+        
+                learnOp(Op.ConstantOperation("π", {M_PI} ))
+    }
+    
+    var variableValues = [String : Double]()
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty
+        {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .ConstantOperation(_, let constant):
+                return (constant(), remainingOps)
+            case .Variable(let variable):
+                return (variableValues[variable], remainingOps)
+                
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate( op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double?
+    {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double?
+    {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func pushOperand(symbol: String) -> Double?
+    {
+        opStack.append(Op.Variable(symbol))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double?
+    {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    func clear() {
+        opStack = []
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Calc/ViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by eduardo on 3/5/15.
+//  Copyright (c) 2015 eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    @IBOutlet weak var history: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var brain = CalcBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func decimal() {
+        
+        if (userIsInTheMiddleOfTypingANumber) {
+            if (display.text!.rangeOfString(".") == nil){
+                display.text = display.text! + "."
+            }
+        } else {
+            display.text = "0."
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+        history.text = "\(brain)"
+    }
+    
+    
+    @IBAction func clear() {
+        displayValue = 0
+        brain.clear()
+        history.text = "\(brain)"
+    }
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if displayValue != nil{
+            if let result = brain.pushOperand(displayValue!) {
+                displayValue = result
+            } else {
+                displayValue = nil // want nil (need dV to be an optional)
+            }
+
+        } else {
+            displayValue = nil // want nil (need dV to be an optional)
+        }
+        history.text = "\(brain)"
+    }
+    var displayValue: Double?{
+        get {
+            if (NSNumberFormatter().numberFromString(display.text!) != nil){
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            } else {
+                return nil
+            }
+        }
+        set {
+            if newValue != nil {
+                display.text = "\(newValue!)"
+                //            userIsInTheMiddleOfTypingNumber = false
+            }
+            else {
+                clear()
+            }
+        }
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
