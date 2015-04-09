94d655337dae92c477a5d4ca65ccacbc0dd43894
kselk <kselker@gmail.com>
2015-04-09 10:20:29 -0600

    Base Support Complete/ Layout/

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -14,6 +14,14 @@ extension Double {
     var toBase10 : String {
         return "\(self)"
     }
+    var toBase16 : String {
+        return String( Int(self) , radix: 16).uppercaseString
+    }
+    
+    var toBase2 : String {
+        return String( Int(self) , radix: 2)
+    }
+
     
 }
 
@@ -21,13 +29,14 @@ extension Double {
 extension String {
     
     var fromBase10toDouble: Double {
-        return NSNumberFormatter().numberFromString(self)!.doubleValue
+        // return NSNumberFormatter().numberFromString(self)!.doubleValue
+        return Double(strtoul(self, nil, 10))
     }
     var fromBase16toDouble: Double {
-        return 1.2
+        return  Double(strtoul(self, nil, 16))
     }
     var fromBase2toDouble: Double {
-        return 101
+        return Double(strtoul(self, nil, 2))
     }
     
 }
@@ -70,10 +79,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func base(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
         currentBase = sender.currentTitle!
-        baseDisplay.text =  currentBase ;
-        enter()
-    }
+        baseDisplay.text =  currentBase
+        }
     
     
     @IBAction func operate(sender: UIButton) {
@@ -95,6 +107,7 @@ class ViewController: UIViewController {
         displayValue = 0
         brain.clear()
         history.text = "\(brain)"
+        userIsInTheMiddleOfTypingANumber = false
     }
     
     
@@ -114,9 +127,7 @@ class ViewController: UIViewController {
     }
     var displayValue: Double?{
         get {
-            
-            if (NSNumberFormatter().numberFromString(display.text!) != nil){
-                switch currentBase {
+            switch currentBase {
                 case "(10)":
                     return display.text!.fromBase10toDouble
                 case "(16)":
@@ -126,15 +137,22 @@ class ViewController: UIViewController {
                 default:
                     return nil
                 }
-                
             }
-            else {
-                return nil
-            }
-        }
+        
         set {
             if newValue != nil {
-                display.text = "\(newValue!)"
+                
+                switch currentBase {
+                case "(10)":
+                    display.text = newValue!.toBase10
+                case "(16)":
+                    display.text = newValue!.toBase16
+                case "(2)":
+                    display.text = newValue!.toBase2
+                default:
+                    display.text = "\(newValue!)"
+                }
+                
                 //            userIsInTheMiddleOfTypingNumber = false
             }
             else {
@@ -143,49 +161,7 @@ class ViewController: UIViewController {
         }
     }
 
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
